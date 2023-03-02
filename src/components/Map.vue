ca4358e528956c2b1612ab72585fd800315fab8b
Gerard Nguyen <gerard@hashicorp.com>
2023-03-03 10:10:21 +1100

    update map zoom level

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,8 +8,9 @@
     <h2 id="contact" class="text-3xl font-bold sm:text-4xl">Contact Us</h2>
   </div>
   <iframe class="w-full"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d64998507.56295651!2d71.15585615!3d6.104549950000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12bf6fbc3599e5%3A0xfc9ae8879384baaf!2sAIG%20Packaging!5e0!3m2!1sen!2sau!4v1677755744480!5m2!1sen!2sau"
-    width="800" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6621.809721231153!2d150.9655718499141!3d-33.917848110084456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12bf6fbc3599e5%3A0xfc9ae8879384baaf!2sAIG%20Packaging!5e0!3m2!1sen!2sau!4v1677798561547!5m2!1sen!2sau"
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
 
 
