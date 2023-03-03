fc1b7af934daff54a8c609fdf5eb1f478b06708b
Gerard Nguyen <gerard@hashicorp.com>
2023-03-04 00:48:38 +1100

    fix typo

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -10,10 +10,10 @@ import Card from './Card.vue';
   <div class="m-4 flex flex-col flex-wrap md:flex-row gap-4 justify-center">
     <Card title="Powder packing" imageUrl="powder_packing.jpeg" />
     <Card title="Sauce packing" imageUrl="sauce_packing.jpeg" />
-    <Card title="Food packing" imageUrl="food_packing.jpeg" />
     <Card title="Nut packing" imageUrl="nuts_packing.jpeg" />
     <Card title="Candy packing" imageUrl="candy_packing.jpeg" />
     <Card title="Liquid packing" imageUrl="liquid_packing.jpeg" />
+    <Card title="Food packaging" imageUrl="food_packing.jpeg" />
   </div>
 </template>
 
